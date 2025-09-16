b94f0dd5331c2b8fc6b75cce3f0194f6e0081038
Olaf <olaf.chuszno@gmail.com>
2025-09-16 22:59:23 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          height: '100vh',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+        </div>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+          <button>test button</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
